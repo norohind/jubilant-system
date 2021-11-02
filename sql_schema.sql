b904e6fe8faceba380973f4242d310744805a35f
norohind <60548839+norohind@users.noreply.github.com>
2021-11-02 19:57:00 +0300

    Add squads_view_2 (current season score, prev season score)

--- sql_schema.sql
+++ sql_schema.sql
@@ -102,3 +102,56 @@ where inserted_timestamp in (
 group by squad_id;
 
 create index idx_squads_states_0 on squads_states (squad_id);
+
+create view if not exists squads_view_2
+as
+select squad_id,
+name,
+tag,
+owner_name,
+owner_id,
+platform,
+created,
+created_ts,
+accepting_new_members,
+power_id,
+power_name,
+super_power_id,
+super_power_name,
+faction_id,
+faction_name,
+user_tags,
+member_count,
+pending_count,
+full,
+public_comms,
+public_comms_override,
+public_comms_available,
+current_season_trade_score,
+previous_season_trade_score,
+current_season_combat_score,
+previous_season_combat_score,
+current_season_exploration_score,
+previous_season_exploration_score,
+current_season_cqc_score,
+previous_season_cqc_score,
+current_season_bgs_score,
+previous_season_bgs_score,
+current_season_powerplay_score,
+previous_season_powerplay_score,
+current_season_aegis_score,
+previous_season_aegis_score,
+current_season_trade_score +
+current_season_combat_score +
+current_season_exploration_score +
+current_season_cqc_score +
+current_season_bgs_score +
+current_season_powerplay_score +
+current_season_aegis_score as current_season_score,
+previous_season_trade_score +
+previous_season_combat_score +
+previous_season_exploration_score +
+previous_season_cqc_score +
+previous_season_bgs_score +
+previous_season_powerplay_score +
+previous_season_aegis_score as previous_season_score, max(inserted_timestamp) as inserted_timestamp from squads_states group by squad_id having tag is not null;
